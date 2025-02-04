7ead2b8d42884b619fe93921578d9d6092a77365
Mamadou Mahadiou Ba <bamahadiou@gmail.com>
2025-02-04 10:21:57 -0500

    considering html files and perfecting main.py file

--- goal_lines/save_line_v1/main.py
+++ goal_lines/save_line_v1/main.py
@@ -2,7 +2,8 @@ import json
 import os
 import sys
 
-def is_python_file(file):
+
+def is_python_or_html_file(file):
     """
     Check if a given file is a Python file.
 
@@ -12,9 +13,7 @@ def is_python_file(file):
     Returns:
         bool: True if the file ends with .py, otherwise False.
     """
-    if os.path.isfile(file):
-        return file.endswith('.py')
-    return False
+    return any([(file.endswith(".py") or file.endswith(".html"))])
 
 
 def is_valid_line(line):
@@ -28,9 +27,7 @@ def is_valid_line(line):
         bool: True if the line is valid, otherwise False.
     """
     line = line.strip()
-    if line.startswith("#") or line.startswith(("'", '"')) or not line:
-        return False
-    return True
+    return not any([line.startswith("#") or line.startswith("<!--") or line.startswith(("'", '"')) or not line])
 
 
 def count_lines_in_file(path):
@@ -46,18 +43,18 @@ def count_lines_in_file(path):
     total_lines = 0
 
     if os.path.isfile(path):
-        with open(path, 'r') as f:
+        with open(path, "r") as f:
             while line := f.readline():
                 if is_valid_line(line):
                     total_lines += 1
 
     elif os.path.isdir(path):
-        ignored_dirs = {'.git', '__pycache__'}
+        ignored_dirs = {".git", "__pycache__"}
         for root, dirs, files in os.walk(path):
             dirs[:] = [d for d in dirs if d not in ignored_dirs]
             for file in files:
                 filepath = os.path.join(root, file)
-                if is_python_file(filepath):
+                if is_python_or_html_file(filepath):
                     total_lines += count_lines_in_file(filepath)
 
     return total_lines
@@ -73,21 +70,17 @@ def inizialize_data(filepath):
     Returns:
         dict: Dictionary containing initialized data.
     """
-    data = {
-        'total_lines': 0,
-        'files': {},
-        'directory': {}
-    }
+    data = {"total_lines": 0, "files": {}, "directory": {}}
 
     if os.path.isfile(filepath):
         new_lines = count_lines_in_file(filepath)
-        data['total_lines'] += new_lines
-        data['files'][filepath] = new_lines
+        data["total_lines"] += new_lines
+        data["files"][filepath] = new_lines
 
     elif os.path.isdir(filepath):
         new_lines = count_lines_in_file(filepath)
-        data['total_lines'] += new_lines
-        data['directory'][filepath] = new_lines
+        data["total_lines"] += new_lines
+        data["directory"][filepath] = new_lines
 
     return data
 
@@ -101,10 +94,11 @@ def save_data(filename, data):
         data (dict): Data to be saved.
     """
     try:
-        with open(filename, 'w') as f:
+        with open(filename, "w") as f:
             json.dump(data, f, indent=4)
     except FileNotFoundError:
-        print(f'File {filename} not found')
+        print(f"File {filename} not found")
+
 
 def load_data(filepath):
     """
@@ -120,7 +114,7 @@ def load_data(filepath):
         return None
 
     try:
-        with open(filepath, 'r') as file:
+        with open(filepath, "r") as file:
             if os.path.getsize(filepath) == 0:
                 print(f"{filepath} is empty.")
                 return None
@@ -129,7 +123,7 @@ def load_data(filepath):
     except json.JSONDecodeError:
         print(f"The file {filepath} contains invalid JSON data.")
         return None
-    
+
 
 def is_new_data_exist(data, new_file):
     """
@@ -142,7 +136,7 @@ def is_new_data_exist(data, new_file):
     Returns:
         bool: True if the file/directory is already recorded, otherwise False.
     """
-    return new_file in data.get('files', {}) or new_file in data.get('directory', {})
+    return new_file in data.get("files", {}) or new_file in data.get("directory", {})
 
 
 def is_data_change(data, new_path):
@@ -159,9 +153,9 @@ def is_data_change(data, new_path):
     new_data_lines = count_lines_in_file(new_path)
 
     if os.path.isfile(new_path):
-        old_data_lines = data['files'][new_path]
+        old_data_lines = data["files"][new_path]
     elif os.path.isdir(new_path):
-        old_data_lines = data['directory'][new_path]
+        old_data_lines = data["directory"][new_path]
 
     return old_data_lines != new_data_lines
 
@@ -179,20 +173,20 @@ def add_new_data_in_file(filename, new_path):
     """
     data_in_file = load_data(filename)
     new_data_lines = count_lines_in_file(new_path)
-    msg = ''
+    msg = ""
 
     if data_in_file is not None:
         if is_new_data_exist(data_in_file, new_path):
             if is_data_change(data_in_file, new_path):
                 if os.path.isfile(new_path):
-                    old_data_lines = data_in_file['files'][new_path]
-                    data_in_file['total_lines'] += new_data_lines - old_data_lines
-                    data_in_file['files'][new_path] = new_data_lines
+                    old_data_lines = data_in_file["files"][new_path]
+                    data_in_file["total_lines"] += new_data_lines - old_data_lines
+                    data_in_file["files"][new_path] = new_data_lines
 
                 elif os.path.isdir(new_path):
-                    old_data_lines = data_in_file['directory'][new_path]
-                    data_in_file['total_lines'] += new_data_lines - old_data_lines
-                    data_in_file['directory'][new_path] = new_data_lines
+                    old_data_lines = data_in_file["directory"][new_path]
+                    data_in_file["total_lines"] += new_data_lines - old_data_lines
+                    data_in_file["directory"][new_path] = new_data_lines
                 save_data(filename, data_in_file)
                 msg = f"Data updated in {filename}"
             else:
@@ -200,12 +194,12 @@ def add_new_data_in_file(filename, new_path):
 
         else:
             if os.path.isfile(new_path):
-                data_in_file['total_lines'] += new_data_lines
-                data_in_file['files'][new_path] = new_data_lines
+                data_in_file["total_lines"] += new_data_lines
+                data_in_file["files"][new_path] = new_data_lines
 
             elif os.path.isdir(new_path):
-                data_in_file['total_lines'] += new_data_lines
-                data_in_file['directory'][new_path] = new_data_lines
+                data_in_file["total_lines"] += new_data_lines
+                data_in_file["directory"][new_path] = new_data_lines
             save_data(filename, data_in_file)
             msg = f"Data saved in file {filename}"
 
@@ -227,7 +221,7 @@ def check_valid_args(paths):
     Returns:
         bool: True if all paths are valid, otherwise False.
     """
-    return all((is_python_file(path) or os.path.isdir(path) for path in paths))
+    return all((is_python_or_html_file(path) or os.path.isdir(path) for path in paths))
 
 
 def main(args):
@@ -238,16 +232,14 @@ def main(args):
         args (list): List of arguments passed via command line.
     """
     if len(args) < 1:
-        sys.exit('Too few arguments!')
+        sys.exit("Too few arguments!")
 
     elif not check_valid_args(args):
-            print("Not all provided paths are valid Python files or directories!")
-            sys.exit(1)
+        print("Not all provided paths are valid Python files or directories!")
+        sys.exit(1)
 
-            
-    filename = 'save_lines.json'
+    filename = "save_lines.json"
     data_in_file = load_data(filename)
-
     if data_in_file is not None:
         for f in args:
             msg = add_new_data_in_file(filename, f)
@@ -257,6 +249,8 @@ def main(args):
         for f in args:
             msg = add_new_data_in_file(filename, f)
         print(f"Initialization: {msg}")
+
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
